0d324ac0f6b1cd88f90d690ece630a1501c7aca9
hugovk <hugovk@users.noreply.github.com>
2015-09-25 17:18:34 +0300

    Initial commit

---
+++ bot_authorise.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+Based on:
+https://gist.github.com/moonmilk/035917e668872013c1bd#gistcomment-1333900
+"""
+from __future__ import print_function, unicode_literals
+import argparse
+import tweepy
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="TODO",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        'consumer_key',
+        help="From your app settings page")
+    parser.add_argument(
+        'consumer_secret',
+        help="From your app settings page")
+    args = parser.parse_args()
+
+    auth = tweepy.OAuthHandler(args.consumer_key, args.consumer_secret)
+    auth.secure = True
+    auth_url = auth.get_authorization_url()
+
+    print('Please authorize: ' + auth_url)
+
+    verifier = raw_input('PIN: ').strip()
+
+    auth.get_access_token(verifier)
+
+    print()
+    print("consumer_key: " + args.consumer_key)
+    print("consumer_secret: " + args.consumer_secret)
+    print("access_token: " + auth.access_token)
+    print("access_token_secret: " + auth.access_token_secret)
+
+# End of file
